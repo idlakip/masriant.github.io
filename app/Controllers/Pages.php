99fb67d12a439c81600f5800dbd16c6ee352eee4
masriant <ssostiami@gmail.com>
2020-06-25 20:42:04 +0700

    Membuat Halaman Pages

---
+++ app/Controllers/Pages.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controllers;
+
+class Pages extends BaseController
+{
+    public function index()
+    {
+        return view('pages/home');
+    }
+
+    public function about()
+    {
+        return view('pages/about');
+    }
+
+    public function contact()
+    {
+        return view('pages/contact');
+    }
+
+
+    //--------------------------------------------------------------------
+
+}
---
+++ app/Views/pages/about.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+</head>
+
+<body>
+    <h2>about me</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas dolor nobis neque sint dolorum quaerat, fugiat ipsum provident. Tempora enim eaque voluptas iusto expedita dicta quis exercitationem sapiente temporibus nobis.</p>
+</body>
+
+</html>
