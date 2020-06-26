dcf3a961c30d700f1f13e7927595884d0465f843
masriant <ssostiami@gmail.com>
2020-06-27 02:57:47 +0700

    tambah template

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -1,8 +1,8 @@
 <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-        <b>Version</b> 3.1.0-pre
+        Rendered in {elapsed_time} seconds <b>Version</b> 1.0.0-Environment: <?= ENVIRONMENT ?> : Masrianto
     </div>
-    <strong>Copyright &copy; 2014-2020 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2014-<?= date('Y') ?> <a href="https://www.lakip.co.id">Admin LAKIP</a>.</strong> All rights reserved.
 </footer>
 
 <!-- Control Sidebar -->
@@ -21,48 +21,6 @@
 <script src="../../dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="../../dist/js/demo.js"></script>
-</body>
-
-</html>
-
-
-
-
-
-<!-- FOOTER: DEBUG INFO + COPYRIGHTS -->
-
-<footer>
-    <div class="environment">
-
-        <p>Page rendered in {elapsed_time} seconds</p>
-
-        <p>Environment: <?= ENVIRONMENT ?></p>
-
-    </div>
-
-    <div class="copyrights">
-
-        <p>&copy; <?= date('Y') ?> CodeIgniter Foundation. CodeIgniter is open source project released under the MIT
-            open source licence.</p>
-
-    </div>
-
-</footer>
-
-<!-- SCRIPTS -->
-
-<script>
-    function toggleMenu() {
-        var menuItems = document.getElementsByClassName('menu-item');
-        for (var i = 0; i < menuItems.length; i++) {
-            var menuItem = menuItems[i];
-            menuItem.classList.toggle("hidden");
-        }
-    }
-</script>
-
-<!-- -->
-
 </body>
 
 </html>
