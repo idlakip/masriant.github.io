cb12314e0b6362a4ac4dbad1bff295d517c55a68
masriant <ssostiami@gmail.com>
2020-06-27 17:27:18 +0700

    tambah routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -33,7 +33,7 @@ $routes->setAutoRoute(true);
 // route since we don't have to scan directories.
 // $routes->get('/', 'Home::index');
 $routes->get('/', 'Pages::index');
-$routes->get('/', 'Komik::index');
+$routes->get('/komik/(:segment)', 'Komik::detail/$1');
 
 /**
  * --------------------------------------------------------------------
