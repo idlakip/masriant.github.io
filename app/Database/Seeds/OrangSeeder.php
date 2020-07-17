c597a506d7db0d9d1050c4e1198631f4f4782978
masriant <ssostiami@gmail.com>
2020-07-17 23:04:47 +0700

    Faker test 1

--- app/Database/Seeds/OrangSeeder.php
+++ app/Database/Seeds/OrangSeeder.php
@@ -9,26 +9,39 @@ class OrangSeeder extends \CodeIgniter\Database\Seeder
 {
   public function run()
   {
+    // $data = [
+    //   [
+    //     'nama' => 'Masrianto SAB',
+    //     'alamat'    => 'Jl. Serdang Baru Raya No. 4B - Bandung',
+    //     'created_at' => Time::now(),
+    //     'updated_at' => Time::now(),
+    //   ],
+    //   [
+    //     'nama' => 'Hasan',
+    //     'alamat'    => 'Jl. Taruna Jaya No. 44 - Solo',
+    //     'created_at' => Time::now(),
+    //     'updated_at' => Time::now(),
+    //   ],
+    //   [
+    //     'nama' => 'Zainal',
+    //     'alamat'    => 'Jl. Serdang Baru IX No. 4B - Makassar',
+    //     'created_at' => Time::now(),
+    //     'updated_at' => Time::now(),
+    //   ]
+    // ];
+
+    // coba faker
+    $faker = \Faker\Factory::create();
+    // generate data by accessing properties
+    // echo $faker->name;
     $data = [
-      [
-        'nama' => 'Masrianto SAB',
-        'alamat'    => 'Jl. Serdang Baru Raya No. 4B - Bandung',
-        'created_at' => Time::now(),
-        'updated_at' => Time::now(),
-      ],
-      [
-        'nama' => 'Hasan',
-        'alamat'    => 'Jl. Taruna Jaya No. 44 - Solo',
-        'created_at' => Time::now(),
-        'updated_at' => Time::now(),
-      ],
-      [
-        'nama' => 'Zainal',
-        'alamat'    => 'Jl. Serdang Baru IX No. 4B - Makassar',
-        'created_at' => Time::now(),
-        'updated_at' => Time::now(),
-      ]
+      'nama'        => $faker->name,
+      'alamat'      => $faker->address,
+      'created_at'  => Time::now(),
+      'updated_at'  => Time::now(),
     ];
+
+
 
     // Simple Queries
     // $this->db->query(
@@ -37,7 +50,7 @@ class OrangSeeder extends \CodeIgniter\Database\Seeder
     // );
 
     // Using Query Builder
-    // $this->db->table('orang')->insert($data);
-    $this->db->table('orang')->insertBatch($data);
+    $this->db->table('orang')->insert($data);
+    // $this->db->table('orang')->insertBatch($data);
   }
 }
